d1da4b46e8ad97d8e00932c3ca4a9cdb507ee265
kainat092002 <kainatmujawar09@gmail.com>
2024-09-27 18:08:55 +0530

    first commit

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id='speciality'>
+        <h1>Find By Speciality</h1>
+        <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free</p>
+
+    </div>
+  )
+}
+
+export default SpecialityMenu
