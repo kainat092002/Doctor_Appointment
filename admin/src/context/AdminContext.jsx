3a5e66e716286e29f443a16ddba2406e6dc381ca
kainat092002 <kainatmujawar09@gmail.com>
2024-10-30 17:02:27 +0530

    changes done

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState } from "react";
 import axios from 'axios'
 import { toast } from 'react-toastify'
-
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,12 +8,11 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL 
-    const [doctors,setDoctors] = useState([])
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [doctors,setDoctors] = useState([])
+    const [token,setToken] = useState('')
+
+    
 
     const getDoctorsData = async () => {
 
@@ -31,6 +30,13 @@ const AppContextProvider = (props) => {
             toast.error(error.message)
         }
 
+    }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
     }
 
         useEffect(()=>{
